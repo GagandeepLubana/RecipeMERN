207b3c6d8c690e3f78263afbf1a2450b4a58ee26
GagandeepLubana <gagandeeplubana1221@gmail.com>
2024-05-26 22:53:54 -0400

    fixed styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import {Home} from "./pages/home"
-import { CreateRecipe } from './pages/create-recipe';
-import { SavedRecipes } from './pages/save-recipe';
-import { Auth } from './pages/auth';
-import { Navbar } from './component/navbar';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./component/navbar";
+import { Auth } from "./pages/auth";
+import { CreateRecipe } from "./pages/create-recipe";
+import { Home } from "./pages/home";
+import { SavedRecipes } from "./pages/saved-recipes";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/auth" element={<Auth />}/>
-          <Route path="/create-recipe" element={<CreateRecipe />}/>
-          <Route path="/save-recipe" element={<SavedRecipes />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipes" element={<SavedRecipes />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </Router>
     </div>
--- client/src/component/navbar.js
+++ client/src/component/navbar.js
@@ -1,11 +1,14 @@
-import React, {useState} from 'react';
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  return (
     <div className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/create-recipe">Create Recipe</Link>
-        <Link to="/saved-recipe">Saved Recipes</Link>
-        <Link to="auth">Login</Link>
+      <Link to="/">Home</Link>
+      <Link to="/create-recipe">Create Recipe</Link>
+      <Link to="/saved-recipes">Saved Recipes</Link>
+    <button> Logout </button>
+      
     </div>
-}
+  );
+};
