d0feb26bff5cb97acc4b635cc795205c0aa4fc21
GagandeepLubana <gagandeeplubana1221@gmail.com>
2024-06-02 16:00:59 -0400

    created recipe component

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -1,6 +1,28 @@
-import React, {useState} from 'react';
-
+import React, { useState } from "react";
 
 export const CreateRecipe = () => {
-    return <div>Home</div>
-}
+    const [recipe, setRecipe] = useState({
+        name: "", 
+        ingredients: [],
+        instructions: "",
+        imageUrl: "",
+        cookingTime: 0, 
+        userOwner: 0,
+    })
+  return (
+    <div className="create-recipe">
+      <h2>Create Recipe</h2>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+        <label htmlFor="name">Ingredients</label>
+        <label htmlFor="instructions">Instructions</label>
+        <textarea id="instructions" name="instructions"></textarea>
+        <label htmlFor="name">name</label>
+        <input type="text" id="imageUrl" name="imageUrl" />
+        <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+        <input type="number" id="cookingTime" name="cookingTime" />
+      </form>
+    </div>
+  );
+};
--- server/src/routes/recipe.js
+++ server/src/routes/recipe.js
@@ -1,6 +1,7 @@
 import express, { response } from "express";
 import mongoose from "mongoose";
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
@@ -14,13 +15,46 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    const recipe = new RecipeModel(req.body)
-    try {
-      const repsonse = await recipe.save();
-      res.json(response);
-    } catch (err) {
-      res.json(err);
-    }
-  });
+  const recipe = new RecipeModel(req.body);
+  try {
+    const repsonse = await recipe.save();
+    res.json(response);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const recipe = await RecipeModel.findById(req.body.recipeId);
+    const user = await UserModel.findById(req.body.userId);
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.json({ savedRecipes: user.savedRecipes });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.get("/savedRecipes/ids", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userId)
+    res.json({savedRecipes: user?.savedRecipes})
+  } catch (err) {
+    res.json(err)
+  }
+})
+
+router.get("/savedRecipes", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userId)
+    const savedRecipes = await RecipeModel.find({
+      _id: {$in: user.savedRecipes}
+    })
+    res.json({savedRecipes})
+  } catch (err) {
+    res.json(err)
+  }
+})
 
 export { router as recipeRouter };
